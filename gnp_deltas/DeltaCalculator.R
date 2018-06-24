e2ca96e42704059e262453feae6861a657a5fdcd
Roberto Bresil <rbresil@motorola.com>
2018-06-24 15:19:19 -0300

    Adding GNP deltas.

--- gnp_deltas/DeltaCalculator.R
+++ gnp_deltas/DeltaCalculator.R
@@ -20,7 +20,6 @@ planilha$Delta_1950_2016 <- as.integer(0)
 
 paises <- as.character(unique(planilha$Entity))
 
-
 for (i in 1:length(paises)) {
   pais <- paises[i]
   valor_1950 = planilha$GDPperCapita[planilha$Entity==pais & planilha$Year==1950]
@@ -33,48 +32,48 @@ for (i in 1:length(paises)) {
   
   # Verifica se os valore existem antes de calcular
   if (length(valor_1950) > 0 & length(valor_1960) > 0) {
-    planilha$Delta_1950_1960[planilha$Entity==pais &planilha$Year==1960] <-
-      valor_1960 - valor_1950
+    x <- round( (((valor_1960 - valor_1950) / valor_1950) * 100), digits = 1)
+    planilha$Delta_1950_1960[planilha$Entity==pais &planilha$Year==1960] <- x
   }
 
   if (length(valor_1960) > 0 & length(valor_1970) > 0) {
-    planilha$Delta_1960_1970[planilha$Entity==pais &planilha$Year==1970] <-
-      valor_1970 - valor_1960
+    x <- round( ((valor_1970 - valor_1960) / valor_1960) * 100, digits = 1)
+    planilha$Delta_1960_1970[planilha$Entity==pais &planilha$Year==1970] <- x
   }
 
   if (length(valor_1970) > 0 & length(valor_1980) > 0) {
-    planilha$Delta_1970_1980[planilha$Entity==pais &planilha$Year==1980] <-
-      valor_1980 - valor_1970
+    x <- round( ((valor_1980 - valor_1970) / valor_1970) * 100, digits = 1)
+    planilha$Delta_1970_1980[planilha$Entity==pais &planilha$Year==1980] <- x
   }
 
   if (length(valor_1950) > 0 & length(valor_1980) > 0) {
-    planilha$Delta_1950_1980[planilha$Entity==pais &planilha$Year==1980] <-
-      valor_1980 - valor_1950
+    x < - round( ((valor_1980 - valor_1950) / valor_1950) * 100, digits = 1)
+    planilha$Delta_1950_1980[planilha$Entity==pais &planilha$Year==1980] <- x
   }
   
   if (length(valor_1980) > 0 & length(valor_1990) > 0) {
-    planilha$Delta_1980_1990[planilha$Entity==pais &planilha$Year==1990] <-
-      valor_1990 - valor_1980
+    x <- round( ((valor_1990 - valor_1980) / valor_1980) * 100, digits = 1)
+    planilha$Delta_1980_1990[planilha$Entity==pais &planilha$Year==1990] <- x
   }
   
   if (length(valor_1990) > 0 & length(valor_2000) > 0) {
-    planilha$Delta_1990_2000[planilha$Entity==pais &planilha$Year==2000] <-
-      valor_2000 - valor_1990
+    x <- round( ((valor_2000 - valor_1990) / valor_1990) * 100, digits = 1)
+    planilha$Delta_1990_2000[planilha$Entity==pais &planilha$Year==2000] <- x
   }
   
   if (length(valor_2000) > 0 & length(valor_2016) > 0) {
-    planilha$Delta_2000_2016[planilha$Entity==pais &planilha$Year==2016] <-
-      valor_2016 - valor_2000
+    x <- round( ((valor_2016 - valor_2000) / valor_2000) * 100, digits = 1)
+    planilha$Delta_2000_2016[planilha$Entity==pais &planilha$Year==2016] <- x
   }
   
   if (length(valor_1980) > 0 & length(valor_2016) > 0) {
-    planilha$Delta_1980_2016[planilha$Entity==pais &planilha$Year==2016] <-
-      valor_2016 - valor_1980
+    x <- round( ((valor_2016 - valor_1980) / valor_1980) * 100, digits = 1)
+    planilha$Delta_1980_2016[planilha$Entity==pais &planilha$Year==2016] <- x
   }
   
   if (length(valor_1950) > 0 & length(valor_2016) > 0) {
-    planilha$Delta_1950_2016[planilha$Entity==pais &planilha$Year==2016] <-
-      valor_2016 - valor_1950
+    x < - round( ((valor_2016 - valor_1950) / valor_1950) * 100, digits = 1)
+    planilha$Delta_1950_2016[planilha$Entity==pais &planilha$Year==2016] <- x
   }
 }
 
